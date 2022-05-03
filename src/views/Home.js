e2dafa8ced74ab30f5050aba686c1d66dfcd6116
kasia-g1 <kasia@DESKTOP-5UM780R>
2022-05-03 15:52:20 +0200

    added calender

--- src/views/Home.js
+++ src/views/Home.js
@@ -14,12 +14,36 @@ const moreButton = Button({
 	},
 });
 
+// let date = new Date();
+// let tdate = date.getDate();
+// let month = date.getMonth() + 1;
+
+// if (tdate < 10) {
+// 	tdate = '0' + tdate;
+// }
+// if (month < 10) {
+// 	month = '0' + month;
+// }
+// let year = date.getUTCFullYear();
+// let minDate = year + '-' + month + '-' + tdate;
+
+// let nextYear = date.getUTCFullYear() + 1;
+// let maxDate = nextYear + '-' + month + '-' + tdate;
+
+// document.getElementsByClassName('arrivalInput').setAttribute('min', minDate);
+
+// document.getElementsByClassName('departureInput').setAttribute('max', maxDate);
+
 export function Home() {
 	const section = document.createElement('section');
 
 	const h1 = document.createElement('h1');
 	const p = document.createElement('p');
 	const divShadow = document.createElement('divShadow');
+
+	const dateArrival = document.createElement('dateArrival');
+	const dateDeparture = document.createElement('dateDeparture');
+	
 
 	divShadow.className = 'hero-shadow ';
 
@@ -34,11 +58,29 @@ export function Home() {
     `;
 
 	p.innerHTML = `
-	<p>Witaj w IT SPA. Każdy programista lubi tu odpoczywać. Zapoznaj się z naszą ofertą.<p>
+	<p>Witaj w IT SPA. Każdy programista lubi tu odpoczywać. Zapoznaj się z naszą ofertą.</p>
 	`;
 	p.className = ' p text-light ';
 
-	section.append(h1, p, divShadow, moreButton);
+	dateArrival.className = 'arrivalInput';
+
+
+	dateArrival.innerHTML = `
+	<form>
+	<label>Rezerwacja od: </label> 
+		<input type="date" >
+	</form>
+	`;
+
+	dateDeparture.className = 'departureInput';
+	dateDeparture.innerHTML = `
+	<form> 
+	<label>Rezerwacja do: </label> 
+		<input type="date" >
+	</form>
+	`;
+
+	section.append(h1, p, divShadow, dateArrival, dateDeparture, moreButton);
 
 	return section;
 }
